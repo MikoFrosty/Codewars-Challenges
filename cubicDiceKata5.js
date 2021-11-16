7a20143f13845bca6e56bf6e6e710290f0f6da5e
brandon <srlguitarist@gmail.com>
2021-11-16 08:09:55 -0500

    Creating file

---
+++ cubicDiceKata5.js
@@ -0,0 +1,17 @@
+// Probabilities for Sums in Rolling Cubic Dice (Kata 5)
+
+
+function rolldiceSumProb(arr, totalSides){
+    var prob;
+    // your code here
+    return prob;
+}
+
+
+
+
+
+
+//console.log(rolldiceSumProb(11, 2)); // Should be 0.055555555555
+//console.log(rolldiceSumProb(8, 2)); // Should be 0.13888888889
+//console.log(rolldiceSumProb(8, 3)); // Should be 0.0972222222222
