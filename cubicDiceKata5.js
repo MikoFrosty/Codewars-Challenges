a119a66019d26b0efe0e48bd45e651c226e01ab5
brandon <srlguitarist@gmail.com>
2021-11-17 08:47:27 -0500

    Added comments for clarification

--- cubicDiceKata5.js
+++ cubicDiceKata5.js
@@ -1,10 +1,15 @@
 // Probabilities for Sums in Rolling Cubic Dice (Kata 5)
 
 
+
 function rolldiceSumProb(arr, totalSides){
-    var prob;
-    // your code here
-    return prob;
+    
+
+    // TODO: find lowest number and highest number and then the possibility
+    // of all numbers in between give x number of 6 sided dice
+    return 6 ** totalSides;
+
+
 }
 
 
@@ -12,6 +17,6 @@ function rolldiceSumProb(arr, totalSides){
 
 
 
-//console.log(rolldiceSumProb(11, 2)); // Should be 0.055555555555
+console.log(rolldiceSumProb(11, 2)); // Should be 0.055555555555
 //console.log(rolldiceSumProb(8, 2)); // Should be 0.13888888889
-//console.log(rolldiceSumProb(8, 3)); // Should be 0.0972222222222
+//console.log(rolldiceSumProb(8, 3)); // Should be 0.0972222222222 
