1f5dc2173c00bd57bac0903b15c5a651997a8a5d
brandon <srlguitarist@gmail.com>
2021-11-18 23:58:26 -0500

    Added recursin code for later studying

--- jsPlayground.js
+++ jsPlayground.js
@@ -3,12 +3,37 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Great example of using recursion to solve a simple dice problem.
+// Study this in more detail at a later time.
+
+/*
 function rolldiceSumProb(arr, totalSides){
     if (arr<totalSides || arr>totalSides*6) return 0;
     if (totalSides===0) return 1;
     let p = 0;
-    for (let i=1; i<=6; i++) p += rolldiceSumProb(arr-i, totalSides-1);
+    for (let i=1; i<=6; i++) p += rolldiceSumProb(arr-i, totalSides-1); 
     return p/6;
 }
 
-console.log(rolldiceSumProb(6,5));
+console.log(rolldiceSumProb(7,2)); 
+*/
