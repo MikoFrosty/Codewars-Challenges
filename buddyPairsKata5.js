02bcdc61bdea8b50870bef94d039a0f9a04897d4
brandon <srlguitarist@gmail.com>
2021-11-22 09:38:46 -0500

    Implement code refactoring part 1

--- buddyPairsKata5.js
+++ buddyPairsKata5.js
@@ -1,11 +1,13 @@
 // Buddy Pairs - Kata 5
 
  
-
+// Refactor for performance
 
 function buddy(start, limit) {
-    const numPairs = [];
-    for (let i = start; i <= limit; i++) {
+    const numPairs = [],
+          iEnd = limit;//*2;
+          //counter = 0;
+    for (let i = start; i<iEnd; i++) {
         const numbers = [1];
         for (let j = 2; ; j++) {
             if (!(i % j)) {
@@ -18,26 +20,37 @@ function buddy(start, limit) {
             else if (j > i / j) { break }
         }
         let tot = numbers.reduce((acc, num) => { return acc + num });
-        if (numbers.length > 1) { numPairs.push([i, tot - 1].sort((a,b) => a-b)) }
+        if (numbers.length > 1) { numPairs.push([i, tot - 1].sort((a,b) => a-b));}// numPairs[numPairs.length-1].push(i); }
+        //while (i>limit && i!===numPairs[counter][1] && i<iEnd && counter<numPairs.length) {counter++}
     }
-    
-    //This part of the code needs work & needs to be as efficient ass possible
-    
-    numPairs.sort((a,b) => a-b);
-    for (let i=0; i<numPairs.length; i++){
-        if (numPairs.includes()){}
+    numPairs.sort();
+    //let pairCandidate = [];
+    for (let i=0; i<numPairs.length-1; i++){
+        if (numPairs[i][0]===numPairs[i+1][0]&&numPairs[i][1]===numPairs[i+1][1]){return numPairs[i];}  //pairCandidate.push(numPairs[i])}
     }
-    
-    console.log(numPairs);
+    //This part of the code needs work & needs to be as efficient as possible
+    // Need to find quickest way to compare numbers in array to find match
+
+    // Too slow
     /*
-    
-    for (let i = 0; i < numPairs.length; i++) {
+     for (let i = 0; i < numPairs.length; i++) {
+        let str = numPairs[i].join('');
         for (let j = i + 1; j < numPairs.length; j++) {
-            if (numPairs[i].includes(numPairs[j])) {
+            if (str === numPairs[j].join('')) {
                 return numPairs[i];
             }
         }
     }
+    */
+    
+     // Slowest
+    /*
+     for (let i=0; i<numPairs.length; i++){
+        let str = numPairs[i].join(' ');  
+        let match = 0;
+        numPairs.forEach(num => num.join(' ')===str?match++:0);
+        if (match===2){return str.split(' ');}           
+    } 
     */
     return 'Nothing';
 }
@@ -45,8 +58,11 @@ function buddy(start, limit) {
 
 // Tests
  
-//console.log(buddy(48, 75));   
-console.log(buddy(23, 100)); // Should be [48, 75]
+console.log(buddy(48, 75));    
+//console.log(buddy(23, 100)); // Should be [48, 75]
 //console.log(buddy(4952, 6539)); // Should be [5775, 6128] 
 //console.log(buddy(381, 4318)); // Should be [1050, 1925]
 //console.log(buddy(2382, 3679)); // Should be "Nothing"
+//console.log(buddy(3234, 9664)); // time = 0.78 seconds
+//console.log(buddy(1071625, 1103735)); // Should be [ 1081184, 1331967 ]   // last two consoles - 14.879
+//console.log(buddy(264211, 267650)); // Should be [266000, 507759]
