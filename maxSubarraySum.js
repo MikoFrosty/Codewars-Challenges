212dc030cef74b8cef7eea3df6b0db50c9e324a0
brandon <srlguitarist@gmail.com>
2021-11-25 07:22:11 -0500

    Kata Completed

--- maxSubarraySum.js
+++ maxSubarraySum.js
@@ -1,19 +1,20 @@
 // Maximum subarray sum - Kata 5
-
+// COMPLETED
 
 
 var maxSequence = function(arr){
-    
-
-
+    let greatestSum = 0;
+    let tempSum = 0;
+    for (let i = 0; i<arr.length; i++){
+        if (tempSum<0){tempSum = 0}
+        tempSum += arr[i];
+        if (tempSum>greatestSum){greatestSum=tempSum}
+    }    
+    return greatestSum;
 }
 
 
 
 
 
-
-
-
-
-//console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // Should be 6
+console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // Should be 6
